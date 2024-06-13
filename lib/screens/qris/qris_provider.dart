40a4b6a7c25e2c6e12832f74d2dbef2a4a227ca2
Kresna <adikresna629@gmail.com>
2024-06-13 09:12:52 +0700

    Branch baru biar gak konflik

--- lib/screens/qris/qris_provider.dart
+++ lib/screens/qris/qris_provider.dart
@@ -3,13 +3,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class QrisProvider with ChangeNotifier {
-
   void home() {
     Modular.to.navigate('/navigationPage');
+  }
+
+  void toPembayaranQRIS() {
+    Modular.to.pushNamed('/PembayaranQRISPage');
+  }
+
+  void toQrisBayarPage() {
+    Modular.to.pushNamed('/QrisBayarPage');
+  }
+
+  void toQRScannerPage() {
+    Modular.to.pushNamed('/QRScannerPage');
   }
 
   @override
   void dispose() {
     // TODO: implement dispose
   }
+
+  toQRISBayarPage() {}
 }
